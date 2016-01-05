daaf680c0dd052926916a59e4e7031f7d74053a6
Kevan Loney <kdloney@gmail.com>
2016-01-05 12:17:36 -0500

    push 1

---
+++ AntonyCleo_Storyboard/Assets/ButtonNextLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class ButtonNextLevel : MonoBehaviour 
+	{
+		public void NextLevelButton(int index)
+	{
+		int i = Application.loadedLevel;
+		Application.LoadLevel (i+1);
+
+	}
+}
